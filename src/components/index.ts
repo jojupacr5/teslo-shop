2c198f5b9776a1028ee63506abab7862d848ae16
jojupacr5 <joseph.paucar@gmail.com>
2024-08-27 00:20:09 -0500

    Se agrego la funcionalidad de cambio de imagen en hover para los productCard y se esta trabajando en el menu sidebar

--- src/components/index.ts
+++ src/components/index.ts
@@ -5,3 +5,6 @@ export * from "./ui/title/Title";
 // Products
 export * from "./products/product-grid/ProductGrid";
 export * from "./products/product-grid/ProductGridItem";
+
+// Sidebar
+export * from "./ui/sidebar/Sidebar";
