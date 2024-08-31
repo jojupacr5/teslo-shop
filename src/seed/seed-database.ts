ddb56f6b83833300f7fe6499342b5b8abdec9833
jojupacr5 <joseph.paucar@gmail.com>
2024-08-31 16:53:30 -0500

    Se termino de configurar el seed del proyecto hacia postgres en docker

--- src/seed/seed-database.ts
+++ src/seed/seed-database.ts
@@ -1,7 +1,52 @@
+import prisma from "../lib/prisma";
 import { initialData } from "./seed";
 
 async function main() {
-  console.log(initialData);
+  // 1. Borrar registros previos
+
+  await prisma.productImage.deleteMany();
+  await prisma.product.deleteMany();
+  await prisma.category.deleteMany();
+
+  const { categories, products } = initialData;
+
+  // Categories
+  const categoriesData = categories.map((category) => ({
+    name: category,
+  }));
+  await prisma.category.createMany({
+    data: categoriesData,
+  });
+
+  const categoriesDB = await prisma.category.findMany();
+  const categoriesMap = categoriesDB.reduce((map, category) => {
+    map[category.name.toLowerCase()] = category.id;
+
+    return map;
+  }, {} as Record<string, string>);
+
+  // Products
+
+  products.forEach(async (product) => {
+    const { type, images, ...rest } = product;
+
+    const dbProduct = await prisma.product.create({
+      data: {
+        ...rest,
+        categoryId: categoriesMap[type],
+      },
+    });
+
+    // Images
+    const imagesData = images.map((image) => ({
+      url: image,
+      productId: dbProduct.id,
+    }));
+
+    await prisma.productImage.createMany({
+      data: imagesData,
+    });
+  });
 
   console.log("Seed ejecutado correctamente");
 }
