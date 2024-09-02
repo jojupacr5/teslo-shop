424c0f7b51c4198b3bc41afa754d558e0f4a4b7b
jojupacr5 <joseph.paucar@gmail.com>
2024-09-01 23:07:03 -0500

    Acomodando el Cart store

--- src/store/cart/cart-store.ts
+++ src/store/cart/cart-store.ts
@@ -3,11 +3,37 @@ import { create } from "zustand";
 
 interface State {
   cart: CartProduct[];
-  // addProductToCart
+  addProductToCart: (product: CartProduct) => void;
   // updateProductQuantity
   // removeProduct
 }
 
-export const useCartStore = create<State>()((set) => ({
+export const useCartStore = create<State>()((set, get) => ({
   cart: [],
+
+  // Methods
+  addProductToCart: (product: CartProduct) => {
+    const { cart } = get();
+
+    // 1. Revisar si el producto existe en el carrito con la talla seleccionada
+    const productInCart = cart.some(
+      (item) => item.id === product.id && item.size === product.size
+    );
+
+    if (!productInCart) {
+      set({ cart: [...cart, product] });
+      return;
+    }
+
+    // 2. Se que el producto existe por talla... tengo que incrementar
+    const updatedCartProducts = cart.map((item) => {
+      if (item.id === product.id && item.size === product.size) {
+        return { ...item, quantity: item.quantity + product.quantity };
+      }
+
+      return item;
+    });
+
+    set({ cart: updatedCartProducts });
+  },
 }));
